49e3c1f818741f29cc24445a20c6d5e3b7486649
Natalia Borodenko <borodenkonatalia9@gmail.com>
2023-10-14 21:06:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10000;
+        int tarif = 20;
+        int bonus = amount / tarif;
+
+        System.out.println("Итоговая сумма миль:" + bonus);
+    }
+
+}git 
